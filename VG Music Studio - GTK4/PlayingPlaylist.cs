f2c0375a88bc597ea00a89f1724b246dbdee902a
PlatinumLucario <silverriolu@hotmail.com>
2024-02-28 22:17:23 +1100

    Updated nuget packages

---
+++ VG Music Studio - GTK4/PlayingPlaylist.cs
@@ -0,0 +1,53 @@
+﻿using Kermalis.VGMusicStudio.Core;
+using Kermalis.VGMusicStudio.Core.Util;
+using Kermalis.VGMusicStudio.GTK4.Util;
+using Gtk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kermalis.VGMusicStudio.GTK4;
+
+internal sealed class PlayingPlaylist
+{
+	public readonly List<int> _playedSongs;
+	public readonly List<int> _remainingSongs;
+	public readonly Config.Playlist _curPlaylist;
+
+	public PlayingPlaylist(Config.Playlist play)
+	{
+		_playedSongs = new List<int>();
+		_remainingSongs = new List<int>();
+		_curPlaylist = play;
+	}
+
+	public void AdvanceThenSetAndLoadNextSong(MainWindow parent, int curSong)
+	{
+		_playedSongs.Add(curSong);
+		SetAndLoadNextSong(parent);
+	}
+	public void UndoThenSetAndLoadPrevSong(MainWindow parent, int curSong)
+	{
+		int prevIndex = _playedSongs.Count - 1;
+		int prevSong = _playedSongs[prevIndex];
+		_playedSongs.RemoveAt(prevIndex);
+		_remainingSongs.Insert(0, curSong);
+		parent.SetAndLoadSequence(prevSong);
+	}
+	public void SetAndLoadNextSong(MainWindow parent)
+	{
+		if (_remainingSongs.Count == 0)
+		{
+			_remainingSongs.AddRange(_curPlaylist.Songs.Select(s => s.Index));
+			if (GlobalConfig.Instance.PlaylistMode == PlaylistMode.Random)
+			{
+				_remainingSongs.Shuffle();
+			}
+		}
+		int nextSong = _remainingSongs[0];
+		_remainingSongs.RemoveAt(0);
+		parent.SetAndLoadSequence(nextSong);
+	}
+}
---
+++ VG Music Studio - GTK4/Program.cs
@@ -0,0 +1,48 @@
+using Adw;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Kermalis.VGMusicStudio.GTK4
+{
+	internal class Program
+	{
+		private readonly Adw.Application app;
+
+		// public Theme Theme => Theme.ThemeType;
+
+		[STAThread]
+		public static int Main(string[] args) => new Program().Run(args);
+		public Program()
+		{
+			app = Application.New("org.Kermalis.VGMusicStudio.GTK4", Gio.ApplicationFlags.NonUnique);
+
+			// var theme = new ThemeType();
+			// // Set LibAdwaita Themes
+			// app.StyleManager!.ColorScheme = theme switch
+			// {
+			//     ThemeType.System => ColorScheme.PreferDark,
+			//     ThemeType.Light => ColorScheme.ForceLight,
+			//     ThemeType.Dark => ColorScheme.ForceDark,
+			//     _ => ColorScheme.PreferDark
+			// };
+			var win = new MainWindow(app);
+
+			app.OnActivate += OnActivate;
+
+			void OnActivate(Gio.Application sender, EventArgs e)
+			{
+				// Add Main Window
+				app.AddWindow(win);
+			}
+		}
+
+		public int Run(string[] args)
+		{
+			var argv = new string[args.Length + 1];
+			argv[0] = "Kermalis.VGMusicStudio.GTK4";
+			args.CopyTo(argv, 1);
+			return app.Run(args.Length + 1, argv);
+		}
+	}
+}
